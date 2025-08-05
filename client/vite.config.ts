c29d442db096fc7eb2514a01dbf3f7c3582b6e1b
Matthew Wang <matthewwang@asana.com>
2025-08-05 14:58:41 -0700

    vite debug config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,13 +12,25 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:3001",
+      '/api': {
+        target: 'http://localhost:3001',
         changeOrigin: true,
-      },
-    },
+        secure: false,      
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
   },
   build: {
     outDir: "../dist/client",
